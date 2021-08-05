46e6cd77f03888b8318e81980e8aadcb1c802063
che369 <63643345+che369@users.noreply.github.com>
2021-08-05 22:57:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,11 +2,11 @@ import os
 
 
 class Config:
-    API_ID = int(os.environ.get("API_ID"))
-    API_HASH = os.environ.get("API_HASH")
-    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
+    API_ID = "1575454" #int(os.environ.get("API_ID"))
+    API_HASH = "66a82fd51b50e02c604e7253b8e27572" #os.environ.get("API_HASH")
+    AUTH_USERS = "209544830" #set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
+    BOT_TOKEN = "1926247211:AAEQqqprWT4buCS0yBQt5ggJnn2pdk7y_5c" #os.environ.get("BOT_TOKEN")
     DB_URI = os.environ.get("DATABASE_URL")
     DOWNLOAD_LOCATION = "./DOWNLOADS"
-    SESSION_NAME = os.environ.get("SESSION_NAME")
+    SESSION_NAME = "MediaInfo_RoBot" #os.environ.get("SESSION_NAME")
     WORKERS = 200
